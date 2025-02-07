82f22958c5a6f3c87572a0933166db68fc8eb086
SAIF_ALABABSEH\sabab <sababseh97@gmail.com>
2025-02-08 01:41:11 +0300

    Removing uneeded lines of code..

--- app/common_components/messageBox.tsx
+++ app/common_components/messageBox.tsx
@@ -4,7 +4,6 @@ function MessageBox() {
     return (
         <div className={styles.messageBox} id={styles.messageBox}>
             <h2 className={styles.messageText} id={styles.messageText}>
-                Test Message
             </h2>
         </div>
     );
