5e1eefce9228973d38f4a690e85bff3bdd8f4cf7
SAIF_ALABABSEH\sabab <sababseh97@gmail.com>
2025-03-03 04:12:38 +0300

    ...

--- app/common_components/messageBox.tsx
+++ app/common_components/messageBox.tsx
@@ -2,10 +2,8 @@ import styles from '../common_css/common.module.css';
 
 function MessageBox() {
     return (
-        <div className={styles.messageBox} id={styles.messageBox}>
-            <h2 className={styles.messageText} id={styles.messageText}>
-            </h2>
-        </div>
+        <h2 className={styles.messageText} id={styles.messageText}>
+        </h2>
     );
 }
 
